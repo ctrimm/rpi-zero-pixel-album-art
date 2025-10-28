1b5b8e54eea2643fddd4d2d8c1f3f77c2e8fe066
Cory Trimm <cory.trimm@gmail.com>
2025-10-27 20:55:01 -0400

    Update __init__.py

--- src/modes/__init__.py
+++ src/modes/__init__.py
@@ -5,5 +5,6 @@ from .sports import SportsMode
 from .clock import ClockMode
 from .pipes import PipesMode
 from .dvd_logo import DVDLogoMode
+from .weather_on_the_8s import WeatherOnThe8sMode
 
 __all__ = ['MusicMode', 'WeatherMode', 'SportsMode', 'ClockMode', 'PipesMode', 'DVDLogoMode', 'WeatherOnThe8sMode']
